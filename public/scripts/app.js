265c73b8a4217d51056d3273b89f4dda57143019
Tamires <tamires.lowande@gmail.com>
2019-10-10 19:42:16 +0000

    code commented

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,32 +1,34 @@
+//making sure the page is loaded before applying the JS logic to it
+$(document).ready(function() {
 
-$(document).ready(function () {
-
-
-  $(document).scroll(function(){
-    var y = $(this).scrollTop()
-    if(y > 200){
-    $('.scroll').fadeIn();
-    $('nav div').fadeOut('slow');
-    } else { 
+//JQuery that shows the side arrow
+  $(document).scroll(function() {
+    let y = $(this).scrollTop();
+    if (y > 200) {
+      $('.scroll').fadeIn();
+      $('nav div').fadeOut('slow');
+    } else {
       $('.scroll').fadeOut('slow');
       $('nav div').fadeIn();
     }
-  })
+  });
 
-  $('.scroll').click(function(){
+  //JQuery that makes the side arrow scroll back to the top
+  $('.scroll').click(function() {
     event.preventDefault();
     $('html, body').animate({scrollTop:0}, 600);
     $('.hide-n-seek').show();
     $('textarea').focus();
-  })
+  });
 
-  $('#action').click(function () {
+  //JQuery event that allows nav message to hide/show the tweeet area
+  $('#action').click(function() {
     $('.hide-n-seek').toggle('slow');
     $('textarea').focus();
-  })
+  });
 
   //This function is Jim's courtesy :)
-  const elapseTime = function (data) {
+  const elapseTime = function(data) {
     let timeDiff = new Date().getTime() - new Date(data).getTime();
     let seconds = timeDiff / 1000;
     let time = seconds;
@@ -43,21 +45,24 @@ $(document).ready(function () {
     } else if (seconds > 60 * 60) {
       time = Math.floor(seconds / 60 / 60);
       return `${time} hour${time > 1 ? 's' : ''} ago`;
-    } else if (seconds > 60 ) {
+    } else if (seconds > 60) {
       time = Math.floor(seconds / 60);
       return `${time} minute${time > 1 ? 's' : ''} ago`;
     } else if (seconds < 60) {
       time = Math.floor(seconds);
       return `${time >= 1 ? time : 1 } second${time > 1 ? 's' : ''} ago`;
     }
-  }
+  };
 
-  const createTweetElement = function (tweet) {
-    const escape = function (str) {
+//function that builds the HTML for each tweet
+  const createTweetElement = function(tweet) {
+
+    //section that prevents any user's post to mess with the HTML rendering
+    const escape = function(str) {
       let div = document.createElement('div');
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
-    }
+    };
 
     const date = elapseTime(tweet.created_at);
 
@@ -83,46 +88,49 @@ $(document).ready(function () {
     </article>`;
 
     return markup;
-  }
+  };
 
-  const renderTweets = function (tweets) {
+   //function that loops over the DB and adds the tweet to the HTML
+  const renderTweets = function(tweets) {
     $('.container #myTweets').empty();
     
     const tweetInput = [];
 
     for (let t of tweets.reverse()) {
-      let chunck = createTweetElement(t)
+      let chunck = createTweetElement(t);
       tweetInput.push(chunck);
     }
     $('.container #myTweets').append(tweetInput.join(''));
-  }
+  };
 
-  $('form').submit(function (event) {
+  //event that prevents default behaviour from form and check for errors when posting new tweets
+  $('form').submit(function(event) {
     event.preventDefault();
     const $tweet = $(this).find('textarea').val();
     if (!$tweet) {
-      $('.error-msg').slideUp(function(){
-        $('.error-msg').text('If You\'re not thinking about anything DON\'T TWEET (please type something)').slideDown()
-      })
+      $('.error-msg').slideUp(function() {
+        $('.error-msg').text('If You\'re not thinking about anything DON\'T TWEET (please type something)').slideDown();
+      });
     } else if ($tweet.length > 140) {
-      $('.error-msg').slideUp(function(){
-      $('.error-msg').text('Oops, for messages greater than 140 characters you should write a book').slideDown()
-      })
+      $('.error-msg').slideUp(function() {
+        $('.error-msg').text('Oops, for messages greater than 140 characters you should write a book').slideDown();
+      });
     } else {
       const input = $(this).serialize();
       $.ajax('/tweets', { method: 'POST', data: input })
         .then(loadTweets);
       $(this).find('textarea').val('');
-      $('.error-msg').slideUp()
+      $('.error-msg').slideUp();
     }
-  })
+  });
 
-  const loadTweets = function () {
+  //function that pass DB information to renderTweets 
+  const loadTweets = function() {
     $.ajax('/tweets', { method: 'GET' })
-      .then(renderTweets)
+      .then(renderTweets);
 
-  }
+  };
 
   loadTweets();
 
-})
+});
